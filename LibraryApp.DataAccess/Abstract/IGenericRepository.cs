7048490a124387978697c278140acad97104aaa7
sefa.maril <56128424+sefamaril@users.noreply.github.com>
2024-02-28 23:52:06 +0300

    GenericRepository interface'i yazıldı ve içeriğindeki kodlar implemente edildi. Exception'lar daha sonra ele alınacak.

---
+++ LibraryApp.DataAccess/Abstract/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace LibraryApp.DataAccess.Abstract
+{
+    public interface IGenericRepository<TEntity, in TKey> where TEntity : BaseEntity<TKey>
+    {
+        Task<bool> AddAsync(TEntity entity, Guid userId);
+
+        Task<bool> UpdateAsync(TEntity entity, Guid userId);
+
+        Task<bool> SoftDeleteAsyncById(TKey id, Guid userId);
+
+        Task<int> GetCountAsync();
+
+        Task<TEntity> GetByIdAsync(TKey id);
+
+        Task<IEnumerable<TEntity>> GetListAsync();
+    }
+}
---
+++ LibraryApp.DataAccess/Concrete/GenericRepository.cs
@@ -0,0 +1,68 @@
+﻿using LibraryApp.DataAccess.Abstract;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryApp.DataAccess.Concrete
+{
+    public class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        private readonly LibraryContext _context;
+
+        public GenericRepository(LibraryContext libraryContext)
+        {
+            _context = libraryContext;
+        }
+
+        public async Task<bool> AddAsync(TEntity entity, Guid userId)
+        {
+            try
+            {
+                entity.CreatedDate = DateTime.UtcNow;
+                entity.CreatedUser = userId;
+
+                await _context.AddAsync(entity);
+                return (await _context.SaveChangesAsync() > 0);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+
+        public async Task<TEntity> GetByIdAsync(TKey id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task<int> GetCountAsync()
+        {
+            return await _context.Set<TEntity>().CountAsync(i => !i.DeletedDate.HasValue);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetListAsync()
+        {
+            return await _context.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<bool> SoftDeleteAsyncById(TKey id, Guid userId)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity == null) return false;
+
+            entity.DeletedDate = DateTime.UtcNow;
+            entity.DeletedUser = userId;
+
+            _context.Attach<TEntity>(entity).State = EntityState.Modified;
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> UpdateAsync(TEntity entity, Guid userId)
+        {
+            entity.ModifiedDate = DateTime.UtcNow;
+            entity.ModifiedUser = userId;
+
+            _context.Update(entity);
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
